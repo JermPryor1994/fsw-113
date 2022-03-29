9e1bf2c8d8ae80b4414daec9c5c6e168eff1769f
JermPryor1994~ <jeremy.pryor@bryanuniversity.edu>
2022-03-29 09:29:53 -0700

    Completed

---
+++ week5/jscript.js
@@ -0,0 +1,35 @@
+// Declare any necessary variables.
+
+// Add an evemt listener that responds to the click of the "print" button by calling a function to instantiate
+//  a new student object, and another function to print the certificate.
+
+// Add an event listener that responds to the click of the reset button by resetting all the values
+// both in the form and in the certificate.
+
+// Create a function that instantiates a new student object with the input from the HTML form.
+
+// Create a function that fills in the student's name, class name, and calculated grade on the certificate .
+
+// Create a function that converts the contents of a comma-separated text string to a numeric array.
+// You can use this function when instantiating the arrays in the student object.
+document.querySelector("#print").addEventListener("click", () => printCert(newStudent()))
+
+document.querySelector("#reset").addEventListener("click", () => {
+    [...document.querySelectorAll("input")].forEach(e => e.value = "");
+    [...document.querySelectorAll(".cert-data")].forEach(e => e.textContent = "");
+})
+function newStudent() {
+    let stuName = document.querySelector("#studentName").value;
+    let courseName = document.querySelector("#className").value;
+    let stuScores = convertToNum(document.querySelector("#studentScores").value);
+    let possScores = convertToNum(document.querySelector("#possibleScores").value);
+    return new Student(stuName, courseName, stuScores, possScores);
+}
+function printCert(cs) {
+    document.querySelector("#certStudentName").textContent = cs.getName();
+    document.querySelector("#certClassName").textContent = cs.getCourse();
+    document.querySelector("#certGrade").textContent = cs.ltrGrade();
+}
+function convertToNum(arr) {
+    return arr.split(",").map(e => parseInt(e));
+}
---
+++ week5/student.js
@@ -0,0 +1,34 @@
+// Create a class called Student whose constructor maintains all four data inputs from the HTML page.
+// The class should also contain the following methods:
+// - a method that adds up all the student's scores
+// - a method that adds up all the possible scores
+// - a method that calculates the student's letter grade (divide the student's score by the possible scores and use the resulting decimal to determine grade)
+
+class Student {
+    constructor(name, course, scores, maxScores) {
+        this.name = name;
+        this.course = course;
+        this.scores = scores;
+        this.maxScores = maxScores;
+    }
+    getName() {
+        return this.name;
+    }
+    getCourse() {
+        return this.course;
+    }
+    sumScores() {
+        return this.scores.reduce((p, c) => (p + c));
+    }
+    possScores() {
+        return this.maxScores.reduce((p, c) => (p + c));
+    }
+    ltrGrade() {
+        let totalScore = (this.sumScores()/this.possScores()) * 100
+        if (totalScore >= 90) return "A";
+        if (totalScore >= 80) return "B";
+        if (totalScore >= 70) return "C";
+        if (totalScore >= 60) return "D";
+        else return "F";
+    }
+}
